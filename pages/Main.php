4bfb979d874e173b35d5000fcac13b8376a627d9
MasterEnotus <Dva-1999@yandex.ru>
2021-08-09 18:13:02 +0400

    Поправил форму страницы, добавил поле для входа (не работает) v0.0.3_09082021

--- pages/Main.php
+++ pages/Main.php
@@ -10,6 +10,44 @@
 	<table border="0" width="100%" cellpadding="5">
 		<tr>
 			<td>
+				<form action="login.php">
+				<table border="0" width="100%" cellpadding="1">
+					<tr>
+						<td width="5%">Login:</td>
+						<td width="5%">
+							<input type="text" name="userlogin" size="10" placeholder="logogin06">
+						</td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+						<td>
+							<input type="password" name="userpswd" size="10">
+						</td>
+						<td></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td align="right">
+							<input type="submit" value="Войти">
+						</td>
+						<td></td>
+					</tr>
+				</table>
+				</form>
+			<td>
+		</tr>
+		<tr>
+			<td>
+			</td>
+		</tr>
+		
+		<tr>
+			<td>
+			</td>
+		</tr>
+		<tr>
+			<td>
 				<button onclick="redirectToRateList();">
 					К списку оценок
 				</button>
@@ -30,6 +68,8 @@
 
 
 	<script>
+		function unknown()
+		{}
 		function redirectToRateList()
 		{
 			window.location = 'http://MyMediaRateProject/pages/RateList.php';
@@ -37,6 +77,12 @@
 		function redirectToExtandedRateList()
 		{
 			window.location = 'http://MyMediaRateProject/pages/ExtendedRateList.php';
+		}
+
+		function randomLogin()
+		{
+			//Пока так
+			return "logogin06";
 		}
 	</script>
 
