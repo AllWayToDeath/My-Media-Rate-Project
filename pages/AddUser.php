10d330f3e67cc7db96391eb77e90cb78e599a628
MasterEnotus <Dva-1999@yandex.ru>
2021-08-12 17:43:12 +0400

    Подготовка для личного кабинета

---
+++ pages/AddUser.php
@@ -0,0 +1,25 @@
+<?
+require_once "../core/start.php";
+require_once "../core/config.php";
+require_once "../core/DBEngine.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset=<?=ENCODING ?>>
+</head>
+<body>
+
+	<table border="1" width="100%" cellpadding="5">
+		<tr>
+			<td>
+				
+			</td>
+		</tr>
+	</table>
+
+</body>
+</html>
